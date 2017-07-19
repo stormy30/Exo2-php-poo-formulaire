6779e22b03b998120bcb7253244855649b8a4aa7
stormy30 <vcoutanceau@outlook.com>
2017-07-19 16:59:51 +0200

    first commit

--- form.php
+++ form.php
@@ -1,7 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: linux
- * Date: 18/07/17
- * Time: 22:27
- */
+
+class form {
+
+    private $data;
+    public $surround ='p';
+
+    public function __construct($data = array()){
+    $this->data = $data;
+     }
+
+    private function surround($html){
+        return"<{$this->surround}>$html</{$this->surround}>";
+    }
+    private function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index] : null;
+    
+    }
+
+    public function input($name)
+    {
+        return $this->surround(
+            '<input type="text" name="' . $name . '"value="' . $this->getValue($name) . '">'
+        );
+
+   }
+
+    public function submit(){
+         return $this->surround( '<button type="submit">Envoyer</button>');
+
+   }
+
+   public function select(){
+        return $this->surround( '<select name="Choix">
+                                                  <option value="choix1">option1</option>
+                                                 <option value="choix2">option2</option>
+                                                 <option value="choix3">option3</option></select>');
+   }
+
+   public function textarea($value,$name){
+       return ('<textarea type="text" name="' .$name . '"value="' . $this->getValue($value) .'">'
+       );
+   }
+
+   }
+
+//   class html{
+//       public function css()
+//       {
+//           return ('<link rel="stylesheet" href="style.css">');
+//       }
+//
+//
+//   }
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: linux
- * Date: 18/07/17
- * Time: 19:05
- */
+
+            <?php
+            require 'form.php';
+            $form = new Form($_POST);
+            ?>
+
+    <form action="#" method="post">
+            <?php
+            echo $form->input('username');
+            echo $form->input('text');
+            echo $form->submit();
+            echo $form->select('option1','option2','option3');
+            echo $form->textarea('votre texte', 'message');
+             ?>
+    </form>
+
+
